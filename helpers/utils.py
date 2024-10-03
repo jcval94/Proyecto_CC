2785e1940dd9a5a48c1a376afb1433d6bc54c628
José Carlos Del Valle <jcval94@gmail.com>
2024-10-02 21:36:59 -0600

    Actualizacion CloudFunction
    Update GCP

--- helpers/utils.py
+++ helpers/utils.py
@@ -9,8 +9,9 @@ def call_predict_function(texto_input):
 
     # URL de tu función desplegada
     # https://us-central1-cloudfunct-435801.cloudfunctions.net/predict_function
-    url = 'https://us-central1-cloudfunct-435801.cloudfunctions.net/predict_function_gen2_0'# 19.65 segundos
+    # url = 'https://us-central1-cloudfunct-435801.cloudfunctions.net/predict_function_gen2_0'# 19.65 segundos
     # url = 'https://us-central1-cloudfunct-435801.cloudfunctions.net/predict_function_0'#51.92 segundos
+    url = 'https://us-central1-cloudfunct-435801.cloudfunctions.net/predict_function_gen2_tiny_0' # 12 segundos
 
     # Datos que quieres enviar, ajusta según el formato esperado por tu función
     data = {
